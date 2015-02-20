1a72be9519265f97f3728622870539bf897a4fc8
Kenneth Butler <kvbutler25@gmail.com>
2015-02-19 19:40:09 -0500

    Changes to be committed: 	modified:   wx-pvtest.py

--- wx-pvtest.py
+++ wx-pvtest.py
@@ -19,32 +19,36 @@ class PVFrame(wx.Frame):
 
 class PVPanel(wx.Panel):	
 	def __init__(self, parent):
-		self.i = 2		
-		self.name = 'baseball'		
+		self.i = 1		
+			
 		wx.Panel.__init__(self, parent, -1, size=(590, 60))
 		self.sizer = wx.BoxSizer(wx.HORIZONTAL)		
 		
 		self.pvName = wx.TextCtrl(self, -1, 'Enter PV name', size=(175,-1), style=wx.TE_PROCESS_ENTER)
-		equals = wx.StaticText(self, -1, "=")
-		self.pvValue = epics.wx.PVText(self, pv=self.name, font=None, fg=None, bg=None, minor_alarm="DARKRED", major_alarm="RED", invalid_alarm="ORANGERED", auto_units=False, units="")
-
+		equals = wx.StaticText(self, -1, '=')
+		
 		self.pvName.Bind(wx.EVT_TEXT_ENTER, self.OnEnter) 				
 
 		self.sizer.Add(self.pvName, 2, wx.EXPAND)
 		self.sizer.Add(equals, 1, wx.EXPAND)
-		self.sizer.Add(self.pvValue, 2, wx.EXPAND)
-
+		
 		self.SetSizer(self.sizer)
 		self.Show(True)
+	
 	def OnEnter(self, e):	
 		name = self.pvName.GetValue()
-		self.name = name
-		self.sizer.Hide(self.i)
+		if self.i >= 2:	
+			self.sizer.Hide(self.i)		
 		self.i+=1
-		pvValue = epics.wx.PVText(self, pv=self.name, font=None, fg=None, bg=None, minor_alarm="DARKRED", major_alarm="RED", invalid_alarm="ORANGERED", auto_units=False, units="")
-		self.sizer.Add(pvValue, 2, wx.EXPAND)
+		pvExists = epics.caget(name)
+		if pvExists != None:
+			pvValue = epics.wx.PVText(self, pv=name, font=None, fg=None, bg=None, minor_alarm="DARKRED", major_alarm="RED", invalid_alarm="ORANGERED", auto_units=False, units="")
+			self.sizer.Add(pvValue, 2, wx.EXPAND)
+		
+		else:
+			self.sizer.Add(wx.StaticText(self, -1, 'PV '+name+' not found.'),2,wx.EXPAND)		
 		self.sizer.Layout()
-		self.Fit()
+		
 
 
 class PVApp(wx.App):
